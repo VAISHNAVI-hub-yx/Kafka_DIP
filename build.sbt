d7bc9702f8351fc94d6686f26afc85c291bfd417
Z00HDDC <vaishnavihp19@gmail.com>
2026-03-16 16:17:23 +0530

    Updated KafkaConsumer.scala

--- build.sbt
+++ build.sbt
@@ -5,5 +5,7 @@ version := "1.0"
 scalaVersion := "2.13.12"
 
 libraryDependencies ++= Seq(
-  "org.apache.kafka" % "kafka-clients" % "3.6.1"
+"org.apache.kafka" % "kafka-clients" % "3.6.1",
+"org.apache.hadoop" % "hadoop-common" % "3.3.6",
+"org.slf4j" % "slf4j-simple" % "2.0.9"
 )
--- src/main/scala/KafkaConsumer.scala
+++ src/main/scala/KafkaConsumer.scala
@@ -1,105 +1,128 @@
-import java.io.File
+import java.util.Properties
+//Kafka Consumer used to consume messages from the topic. It also reads the password from JCEKS file to connect to the Kafka cluster securely.
+//ConsumerRecords is used to store the records fetched from the Kafka topic.
+// The consumer subscribes to the specified topic and continuously polls for new messages, printing them to the console and writing them to a file.
+import org.apache.kafka.clients.consumer.{KafkaConsumer, ConsumerRecords}
+import scala.jdk.CollectionConverters._
+import java.time.Duration
+import java.util.Collections
+import java.io.{PrintWriter, File}
+//import java.time.LocalDateTime
+
+//used to read the password from JCEKS file
+import org.apache.hadoop.conf.Configuration
+//used to read encrypted passwords from JCEKS file
+import org.apache.hadoop.security.alias.CredentialProviderFactory
 
 object KafkaConsumer {
+  // Function to read password from JCEKS safely
+//  conf : Hadoop Configuration object to access the JCEKS file
+//  alias: The alias name of the password in the JCEKS file
+  def getPassword(conf: Configuration, alias: String): String = {
+//    Fetch the password from the JCEKS file using the provided alias. If the alias is not found, throw an exception.
+    val pass = conf.getPassword(alias)
+    if (pass == null) {
+//      if alias is not found in the JCEKS file, it will return null. In that case, we throw a RuntimeException to indicate the issue.
+      throw new RuntimeException("Alias not found in JCEKS: " + alias)
+      }
+//    convert the password from a character array to a string using mkString, which concatenates the characters into a single string.
+    pass.mkString
+    }
+  def main(args: Array[String]): Unit = {
 
-  import java.util.Properties
-//  main kafka class use to read messages from kafka topic
-  import org.apache.kafka.clients.consumer.KafkaConsumer
-//  contains multiple messages fetched from the Kafka topic
-  import org.apache.kafka.clients.consumer.ConsumerRecords
-  import scala.jdk.CollectionConverters._
-  import java.time.Duration
-  import java.util.Collections
-  import java.io.PrintWriter
-  import java.io.File
+    val topic = "vi-sap-settlement-detail"
+//    port 9093 indicates SSL secured Kafka
+    val broker = "kafka-ttc-sapfinance.dev.target.com:9093"
+    val groupId = "apprentice_test"
 
+    // JCEKS file path
+    val jceksPath =
+    "jceks://file///Users/Z00HDDC/IdeaProjects/Kafka/certs/dip_passwords.jceks"
 
+//    creates a Hadoop Configuration object to read the passwords from the JCEKS file.
+//    It sets the path to the JCEKS file in the configuration and then calls the getPassword function to retrieve the truststore and keystore passwords.
+    val conf = new Configuration()
+    conf.set(
+    CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH,
+    jceksPath
+    )
 
-    def main(args: Array[String]): Unit = {
-
-      val topic = "vi-sap-settlement-detail"
-
-      val props = new Properties()
-//kafka server address and port number, group id for the consumer, deserializer for key and value, and auto offset reset policy.
-      // These properties are essential for the consumer to connect to the Kafka cluster and consume messages from the specified topic.
-      props.put("bootstrap.servers", "kafka-ttc-sapfinance.dev.target.com:9093")
-      props.put("group.id", "apprentice_test")
-
-      props.put(
-      "key.deserializer",
-      "org.apache.kafka.common.serialization.StringDeserializer"
-      )
-//Deserializer : As Kafka stores messages as Byte arrays, we need to specify how to convert these bytes back into a usable format. In this case, we are using the StringDeserializer to convert the byte arrays into strings.
-      props.put(
-      "value.deserializer",
-      "org.apache.kafka.common.serialization.StringDeserializer"
-      )
-
-//offset : Each mesasge has a unique number called offset, which represents its position within a partition.
-// read messages from the beginning of the topic if there are no committed offsets for the consumer group.
-      // This means that if the consumer group has not previously consumed any messages from the topic, it will start consuming from the earliest available message in the topic.
-      props.put("auto.offset.reset", "earliest")
-
-
-//      SSL (Secure Sockets Layer) configuration
-      props.put("security.protocol", "SSL")
+    // Read passwords from JCEKS
+    val truststorePassword = getPassword(conf, "src.truststore.pwd")
+    val keystorePassword = getPassword(conf, "src.keystore.pwd")
+    // Key password usually same as keystore password
+    val keyPassword = keystorePassword
 
 
-//      trustore and keystore are used for SSL authentication.
-      //      The truststore contains the certificates of trusted entities,
-      //      while the keystore contains the client's own certificate and private key. By specifying the locations of these files, the consumer can establish a secure connection to the Kafka cluster.
-      props.put(
-        "ssl.truststore.location","/Users/Z00HDDC/IdeaProjects/Kafka/certs/client.truststore (1).jks"
-      )
-      props.put(
-        "ssl.keystore.location","/Users/Z00HDDC/IdeaProjects/Kafka/certs/vend-inc-br3-stage.target.com (1).jks"
-      )
-      props.put("ssl.truststore.password", "/Users/Z00HDDC/IdeaProjects/Kafka/certs/dip_passwords (1).jceks : src.truststore.pwd")
-      props.put("ssl.keystore.password", "/Users/Z00HDDC/IdeaProjects/Kafka/certs/dip_passwords (1).jceks : src.keystore.pwd")
-//creates a new KafkaConsumer instance using the specified properties.
-// This consumer will be used to subscribe to the Kafka topic and consume messages.
-      val consumer = new KafkaConsumer[String, String](props)
+// creates a Properties object to store Kafka configuration
+    val props = new Properties()
+//    specify kafka broker address
+    props.put("bootstrap.servers", broker)
+    props.put("group.id", groupId)
 
-//      subscribe to the specified Kafka topic using the subscribe method of the KafkaConsumer. The subscribe method takes a list of topics as an argument, and in this case, we are subscribing to a single topic named "vi-sap-settlement-detail".
-      consumer.subscribe(Collections.singletonList(topic))
+//    start consuming from the earliest offset if no previous offset is found for the consumer group.
+    //    This ensures that the consumer will read all messages from the beginning of the topic if it's the first time it runs.
+    props.put("auto.offset.reset", "earliest")
+//    Auto commit is enabled, which means the consumer will automatically commit the offsets of messages it has consumed at regular intervals.
+    props.put("enable.auto.commit", "true")
 
-//      create local file named "kafka_output.txt" and a PrintWriter to write the output to that file.
-//      This allows us to save the consumed messages from the Kafka topic into a text file for later analysis or reference.
-      val writer = new PrintWriter(new File("kafka_output.txt"))
+//    Kafka messages are stored as bytes
+    //    deserializers to convert the byte data back into strings for both keys and values.
+    props.put(
+    "key.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer"
+    )
+//convert message value from byte to string
+    props.put(
+    "value.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer"
+    )
 
-      println("Kafka Consumer is running...")
+    // SSL configuration
+    props.put("security.protocol", "SSL")
 
-      while (true) {
-//poll is used to fetch messages from the Kafka topic.
-// It takes a duration as an argument, which specifies how long the consumer should wait for messages to arrive before returning. In this case, it waits for 100 milliseconds.
-// The poll method returns a ConsumerRecords object, which contains all the messages fetched from the topic during that poll.
-        val records: ConsumerRecords[String, String] =
-        consumer.poll(Duration.ofMillis(100))
+    props.put(
+    "ssl.truststore.location",
+    "/Users/Z00HDDC/IdeaProjects/Kafka/certs/client.truststore.jks"
+    )
 
-//        loop through the fetched records and print out the partition, offset, key, and value of each message.
-        for (record <- records.asScala) {
+    props.put(
+    "ssl.keystore.location",
+    "/Users/Z00HDDC/IdeaProjects/Kafka/certs/vend-inc-br3-stage.target.com.jks"
+    )
+//Passwords for the truststore and keystore are read from the JCEKS file to ensure secure handling of sensitive information.
+    props.put("ssl.truststore.password", truststorePassword)
+    props.put("ssl.keystore.password", keystorePassword)
+    props.put("ssl.key.password", keyPassword)
+    props.put("ssl.endpoint.identification.algorithm", "") // Disable hostname verification if needed
 
+// creates a KafkaConsumer instance using the configured properties and subscribes to the specified topic. It then enters an infinite loop where it continuously polls for new messages from the Kafka topic.
+// For each message received, it prints the partition, offset, key, and value to the console and also writes the message value to a file named "kafka_output.txt".
+    val consumer = new KafkaConsumer[String, String](props)
+    consumer.subscribe(Collections.singletonList(topic))
 
-//kafka topic is divided into partitions, and each message within a partition has a unique offset.
-          // the partition and offset information can be useful for tracking the position of the consumer in the topic and for debugging purposes.
-          println("Partition: " + record.partition())
-          println("Offset: " + record.offset())
-//          Key identifies the message and can be used for partitioning and routing messages within Kafka.
-          // The value is the actual content of the message that was produced to the topic.
-          println("Key: " + record.key())
-          println("Value: " + record.value())
-          println("--------------------------------")
+//creates a PrintWriter to write the consumed messages to a file named "kafka_output.txt". The program then enters an infinite loop where it continuously polls for new messages from the Kafka topic.
+// For each message received, it prints the partition, offset, key, and value to the console and also writes the message value to the file.
+    val writer = new PrintWriter(new File("kafka_output.txt"))
+    println("Kafka Consumer Started...")
 
+    while (true) {
+// Fetches records from the Kafka topic with a timeout of 100 milliseconds.
+// If there are new messages, it iterates through each record and prints the partition, offset, key, and value to the console.
+      val records: ConsumerRecords[String, String] =
+      consumer.poll(Duration.ofMillis(100))
+//      loop through the fetched records and print their details to the console.
+      for (record <- records.asScala) {
+        println("Partition: " + record.partition())
+        println("Offset: " + record.offset())
+        println("Key: " + record.key())
+        println("Value: " + record.value())
 
-          writer.println("Partition: " + record.partition())
-          writer.println("Offset: " + record.offset())
-          writer.println("Key: " + record.key())
-          writer.println("Value: " + record.value())
-          writer.println("--------------------------------")
-//          used to ensure that all buffered data is written to the file. This is important because the PrintWriter may buffer the output for performance reasons, and calling flush() ensures that all the buffered data is actually written to the file immediately.
-          writer.flush()
+        writer.println(record.value())
+
+//  flush the writer after writing each message to ensure that the data is written to the file immediately.
+        writer.flush()
         }
-
       }
     }
-  }
+}
